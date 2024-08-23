30e02d3ea4c6d2b7885e9af5bb70c895337cb20e
stman3 <alhodalysalman@gmail.com>
2024-08-23 03:51:55 +0300

    fix some bag. add vpn.

--- src/Components/VPN.jsx
+++ src/Components/VPN.jsx
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const VPN = ({ ipInfo }) => {
-  const [vpnInfo, setVpnInfo] = useState(null);
-
-  useEffect(() => {
-    if (ipInfo) {
-      const fetchVpnInfo = async () => {
-        try {
-          const response = await fetch(`https://vpnapi.io/api/${ipInfo.ip}?key=1d3dcc5bcaff4a81871aedba3ee82f78`);
-          const data = await response.json();
-          setVpnInfo(data);
-        } catch (error) {
-          console.error("Error fetching VPN info:", error);
-        }
-      };
-
-      fetchVpnInfo();
-    }
-  }, [ipInfo]);
-
+const VPN = ({ VpnInfo }) => {
   return (
     <div>
-      {vpnInfo ? (
+      {VpnInfo ? (
         <div>
           <strong>VPN</strong>
-          <p>{vpnInfo.security.vpn ? "The user is using VPN" : "The user is not using VPN"}</p>
+          <p>
+            {VpnInfo.security.vpn
+              ? "The user is using VPN"
+              : "The user is not using VPN"}
+          </p>
         </div>
       ) : (
         <p>Loading VPN info...</p>
